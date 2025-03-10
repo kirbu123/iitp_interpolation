f5b463a83e3461b28e05622ad67add7d15a4dd9e
bunin-kirill <kirbucom@gmail.com>
2025-03-11 02:46:18 +0300

    fix typing 1 time

--- src/iitp_interpolation/utils/utils.py
+++ src/iitp_interpolation/utils/utils.py
@@ -2,7 +2,7 @@ import argparse
 import ast
 
 
-def parse_tuple(string):
+def parse_tuple(string: str):
     try:
         return ast.literal_eval(string)
     except (ValueError, SyntaxError) as e:
